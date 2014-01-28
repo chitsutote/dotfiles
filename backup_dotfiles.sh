eacb498e551400e9c904abac9c354325d1119e63
chitsutote <chitsutote7@gmail.com>
2014-01-28 21:59:45 +0800

    Shell Script for backup dotfiles

---
+++ backup_dotfiles.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+Home="/home/ling"
+Destin="/home/ling/Desktop/CloneRepo/dotfiles"
+
+cp $Home/.vimrc $Destin/vimrc
+cp $Home/.gitconfig $Destin/gitconfig
+cp $Home/.bashrc $Destin/bashrc
